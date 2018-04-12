1f324238f757f911aca3dbcd74e3e847f26d4331
Tammy Tian <tiantammy@outlook.com>
2018-04-13 04:56:01 +0800

    successfully test BASE_URL, and retrieved data, fixed html type on scripts

--- scripts/api.js
+++ scripts/api.js
@@ -1,13 +1,17 @@
 'use strict';
 /* global store, api, bookmark-list*/
-// eslint-disable-next-line no-unused-vars
 
+// eslint-disable-next-line no-unused-vars
 const api = (function(){
-  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/tammy/';
+  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/tammy';
 
   let getBookmarks = function(callback) {
     $.getJSON(`${BASE_URL}/bookmarks`, callback);
   };
 
-  
+  return {
+    getBookmarks,
+    BASE_URL,
+  };
+
 } )();
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,13 @@
 'use strict';
 
 /* global store, api, bookmark-list*/
+
+$(document).ready(function() {
+ 
+});
+
+api.getBookmarks(function(data) {
+  console.log(data);
+});
+
+console.log(api.BASE_URL);
